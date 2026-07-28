05baafce5eb3d2d4655706ba7faadd4a36302286
ideepikadubey <deepikadubey5111@gmail.com>
2026-07-28 15:27:21 +0530

    updated multer file size

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -34,7 +34,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 } // 5MB max
+  limits: { fileSize: 20 * 1024 * 1024 } // 5MB max
 });
 
 module.exports = upload;
